a994d43097aa3a2227462cc4b7d7e76f77b38e51
hoang-quy <quy2529@gmail.com>
2024-04-03 14:30:42 +0700

    init the project

---
+++ src/main/java/com/kiucode/quyhotel/QuyhotelApplication.java
@@ -0,0 +1,13 @@
+package com.kiucode.quyhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuyhotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuyhotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiucode/quyhotel/QuyhotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiucode.quyhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuyhotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
